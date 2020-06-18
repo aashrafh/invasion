a302edf17cf1e73e6edce5d3c7419bb7775ae9bc
Ahmed Ashraf <ahmed.ahmed991@eng-st.cu.edu.eg>
2020-06-18 22:28:44 +0200

    init the project files

---
+++ invasion/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h>Invasion, a game built using React, Redux, and SVG</h>
+    </div>
+  );
+}
+
+export default App;
